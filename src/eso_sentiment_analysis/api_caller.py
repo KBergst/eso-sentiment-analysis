109f421229ac1a13141d38981858c6d7163efb1b
Kendra Bergstedt <kendra.bergstedt@gmail.com>
2025-10-30 12:47:10 -0500

    fleshed out api calling functionality w/ testing

---
+++ src/eso_sentiment_analysis/api_caller.py
@@ -0,0 +1,163 @@
+"""
+Functions involved with calling the ESO forum api.
+"""
+
+import pandas as pd
+from requests_ratelimiter import LimiterSession
+from urllib3.util import Retry
+from requests.adapters import HTTPAdapter
+from datetime import date, datetime, timedelta
+import inspect  # debugging mock issues
+
+def daterange_inclusive(start_date: date, end_date: date):
+    """
+    adapted from https://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python to be inclusive
+    You can tell it isn't my code bc they actually use type hinting B) 
+    I should really do that....
+    """
+    days = int((end_date - start_date).days)+1
+    for n in range(days):
+        yield start_date + timedelta(n)
+
+
+def generate_session(per_minute_limit=100, num_retries=10, status=5, status_forcelist=[504], 
+                     backoff_factor=0.5, raise_on_status=True):
+    """ Create the api session used to make requests. Implements rate limits 
+    and retries on timeout errors.
+    """
+    session = LimiterSession(per_minute=per_minute_limit)
+    retry_obj = Retry(total=num_retries, status=status, status_forcelist = status_forcelist, 
+                      backoff_factor=backoff_factor, raise_on_status=raise_on_status) 
+    session.mount('https://', HTTPAdapter(max_retries=retry_obj))
+
+    return session
+
+def save_to_table(
+    json_response,
+    destination_db_con,
+    destination_table_name,
+    bad_fields=["image", "insertUser", "attributes"],
+):
+    """saves json to table in database, minus some fields"""
+    resp_df = pd.DataFrame(json_response)
+    bad_filter = resp_df.filter(bad_fields)
+    resp_df = resp_df.drop(bad_filter, axis="columns")
+    resp_df.to_sql(
+        destination_table_name, destination_db_con,
+        index=False, if_exists="append"
+    )
+
+    return resp_df
+
+def _save_completed_date(destination_db_con, endpoint, fin_date, dates_table_name="retrieved_dates"):
+    """
+    writes a date and endpoint to a table in a database. For use by get_all_from_one_day 
+    once all the data from the given day has been saved
+    """
+    # don't save if today bc not finished for sure
+    if date.today() <= datetime.strptime(fin_date, "%Y-%m-%d").date():
+        print("not marking present or future date complete; data may be incomplete")
+        return
+    date_df = pd.DataFrame({"endpoint": [endpoint], "date": [fin_date]})
+    date_df.to_sql(
+        dates_table_name, destination_db_con, index=False, if_exists="append"
+    )
+
+                        
+def get_all_from_one_day(
+    api_session,
+    api_base_url,
+    endpoint,
+    date,
+    destination_db_con,
+    destination_table_name,
+    record_limit=100,
+    specific_fields=None,
+    dates_table_name="retrieved_dates",
+    saving_kwargs={},
+):
+    """get all from endpoint on a given day"""
+    #TODO HANDLING TIMEOUTS error 504
+    # handle field spec cases
+    fields_string = ""
+    if not hasattr(specific_fields, "__len__") and (specific_fields is None):
+        pass  # no fields specified
+    elif isinstance(specific_fields, str):
+        # one field specified as string
+        fields_string = "&fields=" + specific_fields  
+    else:  # list of fields
+        fields_string = "&fields=" + ",".join(specific_fields)
+    # pull the data
+
+    # get the first page and send it
+    response = api_session.get(
+        api_base_url + endpoint + "?" + f"limit={record_limit}" + f"&dateInserted={date}" + fields_string, timeout=None
+    )
+    if response.headers["x-app-page-result-count"] == 0:
+        # no data for this day
+        _save_completed_date(destination_db_con, endpoint, date, 
+                            dates_table_name=dates_table_name) 
+        return
+
+    save_to_table(
+        response.json(), destination_db_con, destination_table_name, **saving_kwargs
+    )
+    print(response.headers.keys())
+    # get the remaining pages
+    while "x-app-page-next-url" in response.headers.keys():
+        print("in while loop")
+        response = api_session.get(response.headers["x-app-page-next-url"], timeout=None)
+        save_to_table(
+            response.json(), destination_db_con, destination_table_name, **saving_kwargs
+        )
+    # save date to 'completed' table
+    _save_completed_date(destination_db_con, endpoint, date, dates_table_name=dates_table_name)
+
+
+def get_all_from_endpoint(
+    api_session,
+    api_base_url,
+    endpoint,
+    start_date,
+    destination_db_con,
+    destination_table_name,
+    end_date="today",
+    record_limit=100,
+    specific_fields=None,
+    dates_table_name="retrieved_dates",
+    saving_kwargs={},
+):
+    """
+    start_date: string
+        use YYYY-MM-DD format (RFC3339)
+    record_limit: int, default 100
+        the max number of records to return per api call
+    specific_fields: list-like or None, default None
+        list (or tuple or whatever) of names of specific fields to request from the api
+    """
+    start_date_dt = datetime.strptime(start_date, "%Y-%m-%d").date()
+    # grab today's date if necessary
+    if end_date == "today":
+        end_date_dt = date.today()
+        end_date = end_date_dt.strftime("%Y-%m-%d")
+    else:
+        end_date_dt = datetime.strptime(end_date, "%Y-%m-%d").date()
+    # pull previously finished dates
+    completed_dates = pd.read_sql("SELECT date FROM ? WHERE endpoint is ?", destination_db_con, params=[dates_table_name, endpoint])
+    
+    print(completed_dates)
+    
+    for given_date_dt in daterange_inclusive(start_date_dt, end_date_dt):
+        given_date = given_date_dt.strftime("%Y-%m-%d")
+        if given_date in completed_dates["date"]:
+            print(f"data from {given_date} on {endpoint} endpoint already retrieved, skipping")
+            continue
+        print(f"retrieving data from {given_date}...")
+        get_all_from_one_day(api_session, api_base_url, endpoint, given_date, 
+                             destination_db_con, destination_table_name,
+                             record_limit=record_limit, specific_fields=specific_fields, 
+                             dates_table_name=dates_table_name, saving_kwargs=saving_kwargs)
+    
+    
+
+    
---
+++ test/unit/test_api_caller.py
@@ -0,0 +1,241 @@
+import pandas as pd
+import pytest
+import unittest.mock as mock
+import sqlite3
+from requests_ratelimiter import LimiterSession
+import requests
+import copy
+from datetime import date, datetime
+
+import eso_sentiment_analysis.api_caller as api_caller
+
+example_json_return = [{'commentID': 497925,
+  'discussionID': 61051,
+  'parentRecordType': 'discussion',
+  'parentRecordID': 61051,
+  'name': 'Re: 2-3 second freezes',
+  'categoryID': 277,
+  'body': 'Awesomonium Process is the thing taking up all my CPU',
+  'dateInserted': '2014-03-31T19:44:30+00:00',
+  'dateUpdated': None,
+  'insertUserID': 379598,
+  'updateUserID': None,
+  'score': 0,
+  'depth': 1,
+  'scoreChildComments': 0,
+  'countChildComments': 0,
+  'insertUser': {'userID': 379598,
+   'name': 'alewis478b14_ESO',
+   'url': 'https://forums.elderscrollsonline.com/profile/alewis478b14_ESO',
+   'photoUrl': 'https://us.v-cdn.net/5020507/uploads/bf47ba6b81f347a352defdda0e8d80d5.png',
+   'dateLastActive': '2020-06-10T04:50:20+00:00',
+   'banned': 0,
+   'punished': 0,
+   'private': False,
+   'label': '✭✭✭',
+   'labelHtml': '✭✭✭'},
+  'url': 'https://forums.elderscrollsonline.com/en/discussion/comment/497925#Comment_497925',
+  'type': 'comment',
+  'format': 'BBCode',
+  'attributes': {}},
+ {'commentID': 497926,
+  'discussionID': 61904,
+  'parentRecordType': 'discussion',
+  'parentRecordID': 61904,
+  'name': 'Re: Well done Zenimax',
+  'categoryID': 352,
+  'body': "I was a little upset when I saw some of the quest bugs found during beta hadn't been fixed...  But in general I can't complain, things seem to run better and less laggy than most MMO's that have a much longer history...",
+  'dateInserted': '2014-03-31T19:44:41+00:00',
+  'dateUpdated': None,
+  'insertUserID': 4743204,
+  'updateUserID': None,
+  'score': 0,
+  'depth': 1,
+  'scoreChildComments': 0,
+  'countChildComments': 0,
+  'insertUser': {'userID': 4743204,
+   'name': 'BigDumbViking',
+   'url': 'https://forums.elderscrollsonline.com/profile/BigDumbViking',
+   'photoUrl': 'https://us.v-cdn.net/5020507/uploads/avatarstock/nEOYDZXGZYHM2.png',
+   'dateLastActive': '2014-04-25T12:28:27+00:00',
+   'banned': 0,
+   'punished': 0,
+   'private': False,
+   'label': '✭✭',
+   'labelHtml': '✭✭'},
+  'url': 'https://forums.elderscrollsonline.com/en/discussion/comment/497926#Comment_497926',
+  'type': 'comment',
+  'format': 'BBCode',
+  'attributes': {}},
+ {'commentID': 497927,
+  'discussionID': 62938,
+  'parentRecordType': 'discussion',
+  'parentRecordID': 62938,
+  'name': 'Re: Why limit the vanity pets and treasure maps to one per account?',
+  'categoryID': 195,
+  'body': 'I admit.. if I had known it was just for one character per account..  I too would have not bothered.  Not a very classy move on their part.  I hope they correct that.',
+  'dateInserted': '2014-03-31T19:44:43+00:00',
+  'dateUpdated': None,
+  'insertUserID': 20204,
+  'updateUserID': None,
+  'score': 2,
+  'depth': 1,
+  'scoreChildComments': 0,
+  'countChildComments': 0,
+  'insertUser': {'userID': 20204,
+   'name': 'Davorn',
+   'url': 'https://forums.elderscrollsonline.com/profile/Davorn',
+   'photoUrl': 'https://us.v-cdn.net/5020507/uploads/avatarstock/nALC7UYGT0FJ8.png',
+   'dateLastActive': '2020-02-12T18:48:22+00:00',
+   'banned': 0,
+   'punished': 0,
+   'private': False,
+   'label': '✭✭',
+   'labelHtml': '✭✭'},
+  'url': 'https://forums.elderscrollsonline.com/en/discussion/comment/497927#Comment_497927',
+  'type': 'comment',
+  'format': 'BBCode',
+  'attributes': {}}]
+
+def test_daterange_inclusive():
+    manual_daterange=[date(2015,12,30), date(2015,12,31), date(2016, 1, 1), 
+                      date(2016, 1, 2), date(2016, 1, 3)]
+    daterange=api_caller.daterange_inclusive(date(2015,12,30), date(2016, 1, 3))
+    assert manual_daterange == list(daterange)
+
+
+@pytest.mark.parametrize("test_date,db_written", [(date.today(),False), (date(2015,12,31),True), (date(2100,2,15),False)])
+def test_save_completed_date(mocker, test_date, db_written):
+    mocker.patch('sqlite3.connect', autospec=True)
+    mock_con = sqlite3.connect("eso_forum.db")
+    mock_save_to_db = mocker.patch('pandas.DataFrame.to_sql')
+    str_test_date = test_date.strftime("%Y-%m-%d")
+    
+    api_caller._save_completed_date(mock_con,"test", str_test_date, dates_table_name="test_date")
+
+    if db_written:  # writes data to correct place
+        mock_save_to_db.assert_called_once_with("test_date", mock_con, index=False, if_exists="append")
+    else:  # doesn't write data
+        mock_save_to_db.assert_not_called()
+    
+
+@pytest.mark.parametrize("json_str", [
+    example_json_return,   
+    [{'a':1,'image':1, 'c':1, 'd':1},{'a':1,'image':1, 'c':1, 'd':1}],
+    [{'a':1,'b':1, 'image':1, 'insertUser':1},{'a':1,'b':1, 'image':1, 'insertUser':1}],
+    [{'a':1,'b':1, 'c':1, 'd':1},{'a':1,'b':1, 'c':1, 'd':1}]
+])
+def test_save_to_table(mocker, json_str):
+    mock_save_to_db = mocker.patch('pandas.DataFrame.to_sql')
+    mocker.patch('sqlite3.connect', autospec=True)
+    mock_con = sqlite3.connect("eso_forum.db")
+    
+    test_df_cols = api_caller.save_to_table(json_str, mock_con, "myTable").columns
+    # successfully delete bad_fields but not throw a fit if they don't already exist
+    assert 'image' not in test_df_cols
+    assert 'insertUser' not in test_df_cols
+    assert 'attributes' not in test_df_cols
+    # saves to database and doesn't overwrite what's already there
+    mock_save_to_db.assert_called_once_with("myTable", mock_con, index=False, if_exists="append")
+
+
+@pytest.mark.parametrize("fields_options,expected_path",
+                         [(None, 
+                           "https://forums.elderscrollsonline.com/api/v2/comments?limit=100&dateInserted=2014-06-24"), 
+                          ('dateInserted',
+                           "https://forums.elderscrollsonline.com/api/v2/comments?limit=100&dateInserted=2014-06-24&fields=dateInserted"),
+                          (['private','score'],
+                           "https://forums.elderscrollsonline.com/api/v2/comments?limit=100&dateInserted=2014-06-24&fields=private,score")])
+def test_get_all_from_one_day_fields(mocker, fields_options, expected_path):
+    mocked_api_session = mocker.patch("requests_ratelimiter.LimiterSession", autospec=True)
+    mocker.patch('pandas.DataFrame.to_sql')
+    mocker.patch("eso_sentiment_analysis.api_caller.save_to_table")
+    mocker.patch('sqlite3.connect', autospec=True)
+    mock_con = sqlite3.connect("eso_forum.db")    
+    # returns properly formatted api calling string for each field case
+    api_caller.get_all_from_one_day(mocked_api_session,
+                                    "https://forums.elderscrollsonline.com/api/v2/",
+                                    "comments",
+                                    "2014-06-24",
+                                    mock_con,
+                                    "myTable",
+                                    specific_fields=fields_options)
+    mocked_api_session.get.assert_called_with(expected_path, timeout=None)
+
+    
+@pytest.mark.parametrize("num_pages,header_dicts", 
+                         [(0,[{'x-app-page-result-count':0}]),
+                          (1,[{'x-app-page-result-count':1}]),
+                          (6,[{'x-app-page-result-count':6,"x-app-page-next-url":"next_page"},
+                              {'x-app-page-result-count':6,"x-app-page-next-url":"next_page"},
+                              {'x-app-page-result-count':6,"x-app-page-next-url":"next_page"},
+                              {'x-app-page-result-count':6,"x-app-page-next-url":"next_page"},
+                              {'x-app-page-result-count':6,"x-app-page-next-url":"next_page"},
+                              {'x-app-page-result-count':6}])])
+def test_get_all_from_one_day_pages(mocker, num_pages, header_dicts):
+    # cases: no data from day; one page of data from day; multiple pages of data from day
+    # performs the expected loop given each case
+    # saves to the database the expected number of times given each case
+    mocked_api_session = mocker.patch("requests_ratelimiter.LimiterSession",
+                                      new_callable=mocker.PropertyMock)
+    mocked_response = mocker.patch('requests.Response', new_callable=mocker.PropertyMock)
+    # generate mocked responses which exhibit necessary header qualities to mimic the api
+    mocked_responses=[]
+    for header in header_dicts:
+        response = copy.copy(mocked_response())
+        response.headers = header
+        mocked_responses.append(response)
+    # plug mocked responses into the get fn
+    mocked_api_session.get.side_effect= mocked_responses
+    mocked_save = mocker.patch("eso_sentiment_analysis.api_caller.save_to_table")
+    mocked_date_save = mocker.patch("eso_sentiment_analysis.api_caller._save_completed_date")
+    mocker.patch('sqlite3.connect', autospec=True)
+    mock_con = sqlite3.connect("eso_forum.db")    
+    api_caller.get_all_from_one_day(mocked_api_session,
+                                    "https://forums.elderscrollsonline.com/api/v2/",
+                                    "comments",
+                                    "2014-06-24",
+                                    mock_con,
+                                    "myTable")
+    # retrieved all pages from the api
+    assert mocked_api_session.get.call_count == max(num_pages,1)
+    # saved the data from each api call with some data
+    assert mocked_save.call_count == num_pages
+    # wrote the date once finished
+    mocked_date_save.assert_called_once_with(mock_con, "comments", "2014-06-24",
+                                             dates_table_name="retrieved_dates")
+
+    
+@pytest.mark.parametrize("date_range,end_date", [(['2016-07-24', '2016-08-01'],'2016-08-01'),(['2010-01-01', '2010-01-13'],'2010-01-13'),(['2025-10-24',date.today().strftime("%Y-%m-%d")],'today')])
+def test_get_all_from_endpoint(mocker, date_range, end_date):
+    mocked_api_session = mocker.patch("requests_ratelimiter.LimiterSession")
+    mocker.patch('sqlite3.connect', autospec=True)
+    mock_con = sqlite3.connect("eso_forum.db")  
+    mocked_get_from_day = mocker.patch('eso_sentiment_analysis.api_caller.get_all_from_one_day',
+                                       autospec=True)
+    
+    # generate the expected calls to get_all_from_day
+    mock_completed_dates = {"date":['2016-07-29','2004-04-04']}
+    mock_get_prev_dates = mocker.patch("pandas.read_sql", autospec=True)
+    mock_get_prev_dates.return_value=mock_completed_dates
+    
+    # call the test function
+    api_caller.get_all_from_endpoint(mocked_api_session, 
+                                     "https://forums.elderscrollsonline.com/api/v2/",
+                          "comments", date_range[0], mock_con, "myTable", end_date=end_date,
+                          record_limit=100, specific_fields='dateInserted', 
+                          dates_table_name="dates")
+    # verify correct calls were made
+    expected_calls=[]
+    for given_date in api_caller.daterange_inclusive(datetime.strptime(date_range[0],
+                                                                       "%Y-%m-%d"),
+                                          datetime.strptime(date_range[1], "%Y-%m-%d")):
+        if given_date.strftime("%Y-%m-%d") not in mock_completed_dates["date"]:
+            expected_calls.append(mock.call(mocked_api_session, 
+                                            "https://forums.elderscrollsonline.com/api/v2/",
+                              "comments", given_date.strftime("%Y-%m-%d"), mock_con, "myTable",
+                              record_limit=100, specific_fields='dateInserted', 
+                              dates_table_name="dates", saving_kwargs={}))
+    mocked_get_from_day.assert_has_calls(expected_calls)
+    
+    
